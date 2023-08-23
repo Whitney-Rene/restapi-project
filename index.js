cd85a770f9000eec590afb81a39702ce8a4e4ef2
Whitney-Rene <whtnchambers76@gmail.com>
2023-08-23 17:45:07 -0400

    adds home route/endpoint

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const port = 2023
 
 console.log('Whitney-Rene');
 
+//below are 2 routes (specific endpoints), if I enter anything else (localhost:2023/tech =>"Cannot GET/techtonica")
+//request handler, if someone visits the home of my app, answer them with "hello world"
+//"get" is just showing information
+//"req" ask the server for things, if someone visits the "/", do something
+//"res" if this happens, do this
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
 //this should be the last function of your server, it tells computer which port to use
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
