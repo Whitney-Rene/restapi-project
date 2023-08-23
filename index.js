e04ef65c7cbaffb4c3bfbc0c6fc37e1a720e799d
Whitney-Rene <whtnchambers76@gmail.com>
2023-08-23 17:39:36 -0400

    adds variables for express, the port and app.listen function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('test');
+//app framework (can come in the form of a package, but not all packages are frameworks) for backend
+const express = require('express')
+const app = express()
+
+//arbitrary number for port, can be any number
+const port = 2023
+
+console.log('Whitney-Rene');
+
+//this should be the last function of your server, it tells computer which port to use
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
