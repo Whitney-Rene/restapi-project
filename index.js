3ae9495330d4745b86d080746103158670300b08
Whitney-Rene <whtnchambers76@gmail.com>
2023-08-23 21:51:11 -0400

    begins route for book/params

--- index.js
+++ index.js
@@ -15,10 +15,17 @@ const __dirname = path.resolve(); //similiar to pwd
 
 console.log('Whitney-Rene');
 
-//user will see message
+//home route, user will see message
 app.get('/', (req, res) => {
     console.log("Someone is visiting my page :)")
-    res.send('Hola mundo! You are visiting my server!')
+    res.send('Welcome! You are visiting my server/website!')
+})
+
+//if user goes here and the status code is 200, show this message, still a get request
+app.get('/user', (req, res) => {
+    if(res.statusCode === 200) {
+        res.send("Hello user, nice to see you!")
+    }
 })
 
 //user will see list of books
@@ -27,9 +34,17 @@ app.get('/books', (req, res) => {
 })
 
 //get/list the book in position 2 of the array
-app.get('/books/id1', (req, res) => {
+app.get('/books/secbook' /* DO I NEED A SLASH? /bookssecbook */, (req, res) => {
     res.json(BOOKS[1]);
     // res.json(BOOKS[]) CAN I ONLY HAVE 1 RES?
+})
+
+//pick up at minute 51
+//https://www.dropbox.com/scl/fi/d66uv3noii87bk937p8pp/video1922996227.mp4?dl=0&rlkey=u0l3ogv2res5vlig4ntz1hbca
+app.get('/books/:id' /* wild card for id, this is the route of the url, shows thing after colon */, (req, res) => {
+    const { id } = req.params;  //ID COMES FROM THE URL?
+    console.log(req.params)
+    console.log(id);
 })
 
 //a rule for when the request is not understood
@@ -42,12 +57,3 @@ app.use((req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-
-
-//example of if user goes here and the status code is 200, show this message, still a get request
-// app.get('/user', (req, res) => {
-//     if(res.statusCode === 200) {
-//         res.send("Hello user, nice to see you!")
-//     }
-// })
